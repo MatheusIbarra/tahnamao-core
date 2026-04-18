0e9cbebdbff95add44a209671d88212bc5adc4bf
Matheus <matheusibarra98@gmail.com>
2026-04-18 12:40:19 -0300

    Add production start script and enhance logging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -39,6 +39,8 @@ async function bootstrap(): Promise<void> {
   });
 
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  Logger.log(`Swagger is running on http://localhost:${process.env.PORT ?? 3000}/api/docs`);  Logger.log(`Environment: ${process.env.NODE_ENV ?? 'development'}`);
 }
 
 void bootstrap();
